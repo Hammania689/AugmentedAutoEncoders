9b1efb9d6d1dec757d9b8c8ee16a4f7a6176583b
Hammania689 <hameedabdulrashid@gmail.com>
2022-07-17 04:30:13 -0500

    Refactor to overfitt unit test

--- tests/test_model.py
+++ tests/test_model.py
@@ -13,17 +13,6 @@ from src.aae.models import AugmentedAutoEncoder
 from src.aae.dataset import OnlineRenderer
 from src.aae.TransformUtils import aae_paper_views
 
-def plot_im(im, fig_size:Tuple[int, int]=(60, 30)):
-    fig = plt.figure(figsize=fig_size, tight_layout=True)
-    plt.tick_params(top=False, bottom=False, left=False, right=False, labelleft=False, labelbottom=False)
-    fig.set_tight_layout(True)
-    im = im.transpose(2, 1, 0) 
-    plt.imshow(im)
-    
-    
-def combine_in_out_imgs(imgs: List[np.array], axis: int) -> np.array:                                                                                                                      
-    # NOTE: torch img dim (N, C, H, W) ¬
-    return np.concatenate(imgs, axis=axis)     
 
 class InfiniteIter:
     """
@@ -48,55 +37,35 @@ class InfiniteIter:
 @gin.configurable
 def train_aae(num_workers: int=gin.REQUIRED,
               num_train_iters: int=gin.REQUIRED,
-              save_interval: int=gin.REQUIRED,
-              cache_interval: int=gin.REQUIRED,
+              cache_save_interval: int=gin.REQUIRED,
               device: str=gin.REQUIRED):
     
-    model = AugmentedAutoEncoder()
-    model = model.to(device)
     
     BS = 64
+    batch_iters = 10 #aae_paper_views // BS
+
     dataset = OnlineRenderer()
     single_data_sample = dataset[0]
     infinite_dl = InfiniteIter(single_data_sample, BS)
     data = next(infinite_dl)
-    iters = 200
-    state_dict_hist = []
-    
-    ######################################
-    # Preview Data
-    ######################################
-    view_limit = 1
-    col_stack_view_size = (20, 20)
-    
-    aug, img, _ = data
-    aug_stack = np.split(aug.cpu().numpy(), BS, axis=0)
-    aug_stack = [a.squeeze() for a in aug_stack]
-    aug_col_stack = combine_in_out_imgs(aug_stack[:view_limit], 2)
-    # plot_im(aug_col_stack, col_stack_view_size)
-    plot_im(aug_stack[0])
+
+    model = AugmentedAutoEncoder(fixed_batch=data)
+    model = model.to(device)
     
     ######################################
     # Optimization Step 
     ######################################
-    for idx in tqdm(range(iters)):
+    for epoch in tqdm(range(num_train_iters), desc="AAE Training"):
         
-        cache_recon = (idx % cache_interval == 0)
-        if cache_interval:
-            state_dict_hist.append((idx, model.state_dict))
-        # aug, target, pose = next(infinite_dl)
-        [model.optimize_params(data, device=device, cache_recon=cache_recon) for _ in tqdm(range(aae_paper_views // BS), leave=False)]
-            
-    
-    
-    
-    ckpt_dict = {'model': model.state_dict(),
-                 'prev_ckpts': state_dict_hist,
-                 'dataloader': infinite_dl}
-    return ckpt_dict
+        is_save_epoch = (epoch % cache_save_interval == 0)
 
-# gin.add_config_file_search_path('../')
+        [model.optimize_params(data, device=device, cache_recon=is_save_epoch) for _ in tqdm(range(batch_iters), desc=f"Epoch: {epoch + 1}", leave=False)]
+
+        if is_save_epoch:
+            aux_dict = {'dataLoader': infinite_dl}
+            model.save_state(epoch, aux_dict)
+
+        model.log(epoch, is_save_epoch)
+
 gin.parse_config_file('../config/train/linemod/obj_0001.gin')
-ckpt_dict = train_aae()
-
-torch.save(ckpt_dict,'overfit_on_image.pth')
+train_aae()
