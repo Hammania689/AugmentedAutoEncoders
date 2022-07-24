59ab829d9a725244f6bd2f79f4c3006b2ed6c65b
Hammania689 <hameedabdulrashid@gmail.com>
2022-07-24 16:42:01 -0500

    Fix to codebook creaitng. Str is no longer treated as a path

--- src/aae/models.py
+++ src/aae/models.py
@@ -321,7 +321,7 @@ class AugmentedAutoEncoder(nn.Module):
             codebook_path  = get_path_to_config(target_path)
 
             # Create path if doesn't exist already
-            Path(codebook_path.parent).mkdir(parents=True, exist_ok=True)
+            Path(codebook_path).parent.mkdir(parents=True, exist_ok=True)
 
             torch.save((codebook_cpt, codepose_cpt), codebook_path)
             if self.log_to_wandb:
