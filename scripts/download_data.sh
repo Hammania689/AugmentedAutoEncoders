6c6ab6c3a1f403f36938c789b677f14a6d77daf6
Hammania689 <hameedabdulrashid@gmail.com>
2022-07-27 00:08:13 -0500

    Adding download scripts

---
+++ scripts/download_data.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# download checkpoints
+mkdir -p checkpoints;
+sh ./scripts/gdrive_download.sh 1kBNP_PZ_MakYYBzj58WksFNg5U_c7yjS checkpoints.tar.gz;
+tar -xvf checkpoints.tar.gz -C ./checkpoints/;
+rm ./checkpoints.tar.gz
+
+# download data
+mkdir -p data;
+sh ./scripts/gdrive_download.sh 1-E8786Y2OmgbP09_T6iX9Vq0bbC4c-WA  data.tar.gz;
+tar -xvf data.tar.gz -C ./data/;
+rm ./data.tar.gz
+
+# download detections
+mkdir -p detections;
+sh ./scripts/gdrive_download.sh 1DQC7-ur-3czr5URD7LQF2o3p5j8Nu6AQ detections.tar.gz;
+tar -xvf detections.tar.gz -C ./detections/;
+rm ./detections.tar.gz
+
