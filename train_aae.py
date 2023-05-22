8cfb2ca8ee13357b621e6f008d45767f44174dc4
Hammania689 <hameedabdulrashid@gmail.com>
2023-05-22 07:41:58 -0500

    General and flexible training script

--- train_aae.py
+++ train_aae.py
@@ -1,6 +1,7 @@
 import contextlib
 import sys
 import warnings
+import argparse
 
 import gin
 from tqdm import tqdm
@@ -11,11 +12,10 @@ with warnings.catch_warnings():
     from torchvision.transforms import functional as T
     from src.aae.models import AugmentedAutoEncoder
     from src.aae.Visualizations import plot_batch, plot_img
-    from src.datasets.render_tless_dataset import *
+    from src.datasets.render_fit_dataset import *
     from src.datasets.concat_dataset import ConcatDataset
-    from src.ycb_render.tless_renderer_tensor import *
+    from src.ycb_render.fit_renderer_tensor import *
     from src.config.config import cfg, cfg_from_file, get_output_dir, write_selected_class_file
-
 
 class InfiniteIter:
     """
@@ -59,74 +59,42 @@ def nostdout():
     yield
     sys.stdout = save_stdout
     
-# TODO (ham) make this more general
-@gin.configurable
-def load_ycbrenderer_dataset(dis_dir:     str=gin.REQUIRED,
-                             cfg_file:    str=gin.REQUIRED,
-                             im_dim:      int=gin.REQUIRED,
-                             model_path:  str=gin.REQUIRED,):
 
+@gin.configurable
+def train_aae(dis_dir:     str=gin.REQUIRED,
+                renderer:    torch.utils.data.Dataset=gin.REQUIRED,
+                cfg_file:    str=gin.REQUIRED,
+                im_dim:      int=gin.REQUIRED,
+                im_scale:  float=gin.REQUIRED,
+                model_path:  str=gin.REQUIRED,
+                num_workers: int=gin.REQUIRED,
+                num_train_iters: int=gin.REQUIRED,
+                cache_save_interval: int=gin.REQUIRED,
+                batch_size: int=gin.REQUIRED,
+                device: str=gin.REQUIRED):
+    
+    print("Loading Data....")
     cfg_from_file(cfg_file)
     cfg.MODE = 'TRAIN'
     cfg.TRAIN.RENDER_SZ = im_dim
     cfg.TRAIN.INPUT_IM_SIZE = [im_dim, im_dim]
 
-    print(cfg.TRAIN.OBJECTS)
-    print(cfg.TRAIN.RENDER_SZ)
-    print(cfg.TRAIN.INPUT_IM_SIZE)
+    cfg.TRAIN.ROI_CENTER_RANGE = [0, 0, im_dim // 2, im_dim //2]
+    cfg.TRAIN.ROI_SIZE_RANGE   = [im_dim * (1 - im_scale), im_dim * (1 + im_scale)]
+    
+    with nostdout():
+        # augmented dataset
+        # dataset_train = fit_multi_render_dataset(model_path, cfg.TRAIN.OBJECTS,
+        dataset_train = renderer(model_path, cfg.TRAIN.OBJECTS,
+                                                 render_size=cfg.TRAIN.RENDER_SZ,
+                                                 output_size=cfg.TRAIN.INPUT_IM_SIZE)
 
-    # set up render
-    models = cfg.TRAIN.OBJECTS[:]
-    renderer = TLessTensorRenderer(cfg.TRAIN.RENDER_SZ, cfg.TRAIN.RENDER_SZ)
-    if cfg.TRAIN.USE_OCCLUSION:
-        with open('./src/datasets/tless_classes.txt', 'r') as class_name_file:
-            class_names_all = class_name_file.read().split('\n')
-            for class_name in class_names_all:
-                if class_name not in models:
-                    models.append(class_name)
-
-        class_colors_all = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255),
-                            (128, 0, 0), (0, 128, 0), (0, 0, 128), (128, 128, 0), (128, 0, 128), (0, 128, 128),
-                            (64, 0, 0), (0, 64, 0), (0, 0, 64), (64, 64, 0), (64, 0, 64), (0, 64, 64),
-                            (155, 0, 0), (0, 155, 0), (0, 0, 155), (155, 155, 0), (155, 0, 155), (0, 155, 155),
-                            (200, 0, 0), (0, 200, 0), (0, 0, 200), (200, 200, 0),
-                            (200, 0, 200), (0, 200, 200)
-                            ]
-
-
-        obj_paths = ['{}/tless_models/{}.ply'.format(model_path, item) for item in models]
-        texture_paths = ['' for cls in models]
-        renderer.load_objects(obj_paths, texture_paths, class_colors_all)
-        renderer.set_projection_matrix(cfg.TRAIN.RENDER_SZ, cfg.TRAIN.RENDER_SZ, cfg.TRAIN.FU, cfg.TRAIN.FV,
-                                       cfg.TRAIN.RENDER_SZ/2.0, cfg.TRAIN.RENDER_SZ/2.0, 0.01, 10)
-        renderer.set_camera_default()
-        renderer.set_light_pos([0, 0, 0])
-
-    # synthetic dataset
-    dataset_train = tless_multi_render_dataset(model_path, cfg.TRAIN.OBJECTS, renderer,
-                                             render_size=cfg.TRAIN.RENDER_SZ,
-                                             output_size=cfg.TRAIN.INPUT_IM_SIZE)
-
-    # background dataset
+    # real image background dataset
     dataset_dis = DistractorDataset(dis_dir, cfg.TRAIN.CHM_RAND_LEVEL,
                                     size_crop=(cfg.TRAIN.INPUT_IM_SIZE[1],
                                                cfg.TRAIN.INPUT_IM_SIZE[0]))
     
-    return dataset_train, dataset_dis
-
-
-@gin.configurable
-def train_aae(num_workers: int=gin.REQUIRED,
-              num_train_iters: int=gin.REQUIRED,
-              cache_save_interval: int=gin.REQUIRED,
-              batch_size: int=gin.REQUIRED,
-              device: str=gin.REQUIRED):
     
-    
-    print("Loading Data....")
-    with nostdout():
-        dataset_train, dataset_dis = load_ycbrenderer_dataset()
-
     dl = DataLoader(dataset_train,
                    batch_size=batch_size,
                    shuffle=True,
@@ -139,6 +107,7 @@ def train_aae(num_workers: int=gin.REQUIRED,
 
 
     def produce_augmented_data(data, bg_dl, device):
+        
         im, target, _, mask, _, _, _, _, _, roi_affine, *_= data
         
         im         = im.to(device)
@@ -152,24 +121,18 @@ def train_aae(num_workers: int=gin.REQUIRED,
         mask   = 1 - mask
         
 
-        cur_batch_sz    = im.size(0)
+        cur_batch_sz    = images.size(0)
         bg              = bg_dl.__next__(cur_batch_sz).to(device)
-        imdisp          = im + mask * bg[:cur_batch_sz]
+        imdisp          = images + (mask * bg[:cur_batch_sz])
         noise_level     = np.random.uniform(0, 0.05)
         imdisp          += torch.randn_like(imdisp) * noise_level
         
         return (imdisp, target)
     
-    
-    for rend in tqdm(dl, desc="AAE Training"):
-        fixed_data = produce_augmented_data(rend, bg_dl, device)
-        break
-    
-        
+    fixed_data = produce_augmented_data(next(iter(dl)), bg_dl, device)
     model = AugmentedAutoEncoder(fixed_batch=fixed_data)
     model = model.to(device)
-    cache_save_interval = 2
-
+    
     ######################################
     # Optimization Step 
     ######################################
@@ -190,6 +153,23 @@ def train_aae(num_workers: int=gin.REQUIRED,
         
     return model
     
-        
-gin.parse_config_file('./config/train/linemod/obj_0001.gin')
-model = train_aae()
+
+
+def parse_args():
+    """
+    parse input arguments
+    """
+    parser = argparse.ArgumentParser(description='train aae model for a certain object from FIT parts')
+    parser.add_argument('--gin_cfg_dir', dest='gin_cfg_dir',
+                        help='directory for configuration file',
+                        required=True, type=str)
+    
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    gin.parse_config_file(args.gin_cfg_dir)
+    model = train_aae()
+    
+
